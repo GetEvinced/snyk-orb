3823f92709700cccef2f68875631627b36904570
Michael Bronfman <michael.bronfman@evinced.com>
2024-01-16 10:33:54 +0200

    fix

--- src/scripts/download_and_configure_snyk.sh
+++ src/scripts/download_and_configure_snyk.sh
@@ -12,6 +12,13 @@ fi
 
 if [[ ! -x "/tmp/snyk" ]] || <<parameters.no-cache>> ]]; then
   SNYK_CLI_VERSION=$(cat "/tmp/.snyk-version")
-  echo "Downloading Snyk CLI version <span class="math-inline">\{SNYK\_CLI\_VERSION\}"
-curl \-O \-\-compressed \-\-retry 6 \-\-retry\-all\-errors \[https\://static\.snyk\.io/cli/v\]\(https\://static\.snyk\.io/cli/v\)</span>{SNYK_CLI_VERSION}/snyk-<<parameters.os>> || curl -O --compressed --retry 6 [https://static.snyk.io/cli/v](https://static.snyk.io/cli/v)${SNYK_CLI_VERSION}/snyk-<<parameters.os>>
-  curl -O --compressed --retry 6 --retry-all-
+  echo "Downloading Snyk CLI version <span class="math-inline">{SNYK_CLI_VERSION}"
+    curl -O --compressed --retry 6 --retry-all-errors https://static.snyk.io/cli/v${SNYK_CLI_VERSION}/snyk-<<parameters.os>> || curl -O --compressed --retry 6 https://static.snyk.io/cli/v${SNYK_CLI_VERSION}/snyk-<<parameters.os>>
+    curl -O --compressed --retry 6 --retry-all-errors https://static.snyk.io/cli/v${SNYK_CLI_VERSION}/snyk-<<parameters.os>>.sha256 || curl -O --compressed --retry 6 https://static.snyk.io/cli/v${SNYK_CLI_VERSION}/snyk-<<parameters.os>>.sha256
+    sha256sum -c snyk-<<parameters.os>>.sha256
+    sudo mv snyk-<<parameters.os>> /tmp/snyk
+    sudo chmod +x /tmp/snyk
+fi
+sudo ln -sf /tmp/snyk /usr/local/bin/snyk
+snyk config set disableSuggestions=true
+<<#parameters.token-variable>>snyk auth $<<parameters.token-variable>><</parameters.token-variable>>
