6591465e314b5cf40c9ca5951e01deb09765dba4
Michael Bronfman <michael.bronfman@evinced.com>
2024-01-16 10:41:08 +0200

    fix

--- src/scripts/retrieve_snyk_version.sh
+++ src/scripts/retrieve_snyk_version.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [[ -z "${SNYK_CLI_VERSION}" ]]; then  # Removed extra closing brackets and ensured variable interpolation
-  curl --compressed --retry 6 --retry-all-errors \
-    https://static.snyk.io/cli/latest/version > /tmp/.snyk-version || curl --compressed --retry 6 https://static.snyk.io/cli/latest/version > /tmp/.snyk-version 
+if [[ -z "${SNYK_CLI_VERSION}" ]]; then
+  curl --compressed --retry 6 --retry-all-errors https://static.snyk.io/cli/latest/version > /tmp/.snyk-version || curl --compressed --retry 6 https://static.snyk.io/cli/latest/version > /tmp/.snyk-version 
 else
   echo "${SNYK_CLI_VERSION}" >> /tmp/.snyk-version
 fi
-
 cat /tmp/.snyk-version
