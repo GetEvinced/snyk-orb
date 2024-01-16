220f71484c8bab2461d12f2663177ece438a7e62
Michael Bronfman <michael.bronfman@evinced.com>
2024-01-16 10:30:27 +0200

    fix

--- src/scripts/retrieve_snyk_version.sh
+++ src/scripts/retrieve_snyk_version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ -z "<span class="math-inline">\{SNYK\_CLI\_VERSION\}" ]]; then
-curl \-\-compressed \-\-retry 6 \-\-retry\-all\-errors \\
-\[https\://static\.snyk\.io/cli/latest/version\]\(https\://static\.snyk\.io/cli/latest/version\) \> /tmp/\.snyk\-version \\
-\|\| curl \-\-compressed \-\-retry 6 \\
-\[https\://static\.snyk\.io/cli/latest/version\]\(https\://static\.snyk\.io/cli/latest/version\) \> /tmp/\.snyk\-version
+if [[ -z "<span class="math-inline">{SNYK_CLI_VERSION}" ]]; then
+curl --compressed --retry 6 --retry-all-errors 
+[https://static.snyk.io/cli/latest/version](https://static.snyk.io/cli/latest/version) > /tmp/.snyk-version 
+|| curl --compressed --retry 6 
+[https://static.snyk.io/cli/latest/version](https://static.snyk.io/cli/latest/version) > /tmp/.snyk-version
 else
 echo "</span>{SNYK_CLI_VERSION}" >> /tmp/.snyk-version
 fi
