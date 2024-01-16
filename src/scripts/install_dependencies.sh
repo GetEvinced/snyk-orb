2511a6343ecc5b9f1003931db95a866cd472c44c
Michael Bronfman <michael.bronfman@evinced.com>
2024-01-16 10:49:41 +0200

    delete

--- src/scripts/install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "<<parameters.os>>" == "alpine" && "<<parameters.install-alpine-dependencies>>" == "true" ]];
- then
-  # Install required dependencies for Alpine Linux
-  apk add -q --no-progress --no-cache curl wget libstdc++ sudo
-fi
--- src/scripts/retrieve_snyk_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ -z "${SNYK_CLI_VERSION}" ]]; then
-  curl --compressed --retry 6 --retry-all-errors https://static.snyk.io/cli/latest/version > /tmp/.snyk-version || curl --compressed --retry 6 https://static.snyk.io/cli/latest/version > /tmp/.snyk-version 
-else
-  echo "${SNYK_CLI_VERSION}" >> /tmp/.snyk-version
-fi
-cat /tmp/.snyk-version
